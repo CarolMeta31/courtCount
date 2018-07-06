cabe8772fb079da1c271e5e1a70b804f20858ed5
caro <nxaba31@gmail.com>
2018-07-06 09:17:23 +0200

    court commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WinnersPage } from '../winners/winners';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  scoreForTeamA=0;
+scoreForTeamB=0;
+
   constructor(public navCtrl: NavController) {
 
   }
-
+  teamA(value:number):void{
+    this.scoreForTeamA +=value;
+    
+  // this.scoreForTeamA =this.scoreForTeamA + value;
+  }
+  teamB(value:number):void{
+    this.scoreForTeamB +=value;
+    
+  // this.scoreForTeamA =this.scoreForTeamB + value;
+  }
+  reSet(value:number):void{
+    this.scoreForTeamA=value;
+    this.scoreForTeamB =value;
+  }
+ endMatch():void{
+   this.navCtrl.push(WinnersPage);
+ }
 }
+
